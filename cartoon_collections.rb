544f01fd15a60e4409dc50a40b328186227440bf
Joseph Ruiz <ruizj3@uw.edu>
2019-11-19 01:38:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,8 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.map { |call| call.capitalize + '!' }
 end
 
+summon_captain_planet(planeteer_calls)
+
 def long_planeteer_calls# code an argument here
   # Your code here
 end
